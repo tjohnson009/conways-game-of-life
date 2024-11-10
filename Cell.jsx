cf08ac6904be4ad8c14f36a211d46b0dd1e6cb01
Tim Johnson <62769082+tjohnson009@users.noreply.github.com>
2024-11-10 16:18:04 -0500

    Implement hexagon drawing via canvas for cells appearance

--- Cell.jsx
+++ Cell.jsx
@@ -4,8 +4,18 @@ export default class Cell {
     static DEAD_COLOR = 'rgb(8,8,8)'; 
     static ALIVE_COLOR = '#F8F8FF'; 
 
-    constructor() {
+    constructor(context, startX, startY) {
         this.status = 0; // 0 = dead, 1 = alive
+        this.context = context; 
+        this.startX = startX; 
+        this.startX = startX; 
+        this.startY = startY; 
+    }
+    // ctx.fillStyle = Cell.ALIVE_COLOR; 
+    // ctx.fillRect(50, 50, 10, 10); 
+
+    getStatus() {
+        return this.status; 
     }
 
     handleClick() {
@@ -19,5 +29,24 @@ export default class Cell {
 
     die() {
         this.status = 0; 
+    }
+
+    drawHexagonCell(context, centerX, centerY) {
+        const a = 2 * Math.PI / 6;
+        const r = Cell.height / 2;
+
+        context.beginPath(); 
+        for(let i = 0; i < 6; i++) { 
+            if (i === 0) {
+                context.moveTo(centerX + r * Math.cos(a * i), centerY + r * Math.sin(a * i)); 
+            } else {
+                context.lineTo(centerX + r * Math.cos(a * i), centerY + r * Math.sin(a * i)); 
+            }
+        }
+        context.closePath(); 
+        context.stroke();  
+        context.fillStyle = Cell.ALIVE_COLOR; 
+        // context.fill(); 
+        console.log('Hexagon cell drawn'); 
     }
 }
--- Gameboard.jsx
+++ Gameboard.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import Cell from "./Cell";
 
 export default function Gameboard() {
     // aspect ratio is 16:10 - 80 cells x 128 cells for 10 x 10 cells
@@ -13,14 +14,20 @@ export default function Gameboard() {
         ctx.fillStyle = 'rgb(8,8,8)';
         ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
-        // ctx.fillStyle = ALIVE_COLOR; 
-        // ctx.fillRect(50, 50, 10, 10)
+        // ctx.fillStyle = Cell.ALIVE_COLOR; 
+        // ctx.fillRect(50, 50, 10, 10); 
+        let test = new Cell(ctx, 50, 50); 
+        // ctx.fillStyle = Cell.ALIVE_COLOR; 
+        test.drawHexagonCell(ctx, 150, 150); 
+        ctx.fill(); 
     })
+
+    //fill the canvas with cells
         
     return (
         <>
         {/* <h1>This is the gameboard. We&apos;re gonna do Canvas for this one...</h1> */}
-        <canvas id='canvas' width={CANVAS_WIDTH} height={CANVAS_HEIGHT}></canvas>
+        <canvas id='canvas' width={CANVAS_WIDTH} height={CANVAS_HEIGHT} className=''></canvas>
         </>
     )
 }
