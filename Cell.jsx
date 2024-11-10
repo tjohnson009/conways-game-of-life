afbc0f00235591eba56c4cd0ec192085b7ae819a
Tim Johnson <62769082+tjohnson009@users.noreply.github.com>
2024-11-09 20:49:20 -0500

    Update gameboard and cell code

--- Cell.jsx
+++ Cell.jsx
@@ -1,12 +1,16 @@
 export default class Cell {
+    static height = 10;
+    static width = 10;  
+    static DEAD_COLOR = 'rgb(8,8,8)'; 
+    static ALIVE_COLOR = '#F8F8FF'; 
+
     constructor() {
         this.status = 0; // 0 = dead, 1 = alive
-        this.height = 10;
-        this.width = this.height;  
     }
 
     handleClick() {
         // makes the cell alive during the simulation
+        this.comeAlive(); 
     }
 
     comeAlive() {
--- Gameboard.jsx
+++ Gameboard.jsx
@@ -1,16 +1,20 @@
 import { useEffect } from "react";
 
 export default function Gameboard() {
-    // aspect ratio is 16:10 - 50 cells x 80 cells
+    // aspect ratio is 16:10 - 80 cells x 128 cells for 10 x 10 cells
     const CANVAS_WIDTH = 1280; 
     const CANVAS_HEIGHT = 800; 
 
+    // draw the canvas
     useEffect(() => {
         const canvas = document.getElementById("canvas");
         const ctx = canvas.getContext("2d");
         
-        ctx.fillStyle = "rgb(8,8,8)";
+        ctx.fillStyle = 'rgb(8,8,8)';
         ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+        // ctx.fillStyle = ALIVE_COLOR; 
+        // ctx.fillRect(50, 50, 10, 10)
     })
         
     return (
