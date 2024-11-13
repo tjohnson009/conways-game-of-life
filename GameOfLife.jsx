607936405e7455c2298ccf5e2362908886201eb1
Tim Johnson <62769082+tjohnson009@users.noreply.github.com>
2024-11-13 11:06:10 -0500

    Fix issue with useEffect and add canvas/context state

--- GameOfLife.jsx
+++ GameOfLife.jsx
@@ -2,44 +2,52 @@ import { useEffect, useState } from "react";
 import Cell from "./Cell";
 
 export default function GameOfLife() {
-    // aspect ratio is around 16:10 - 80 cells x 128 cells for 10 x 10 cells
+    // aspect ratio is around 16:10 - 76 cells x 128 cells for 20 x 20 cells - 2342 total cells
     const CANVAS_WIDTH = 1280; 
     const CANVAS_HEIGHT = 760; 
     const [gameboard, setGameboard] = useState([]);
-    let numRows = CANVAS_HEIGHT / Cell.height;
-    let numCols = CANVAS_WIDTH / Cell.width;
-
-    // initialize cells
-    useEffect(() => {
-        for (let y = 0; y < numRows; y++) {
-            for (let x = 0; x < numCols; x++) {
-                gameboard.push(new Cell(this.context, x, y));
-            }
-        }
-    }, [])
-
-    // draw the canvas
+    const [canvasState, setCanvasState] = useState({ canvas:null, context: null }); 
+    // const canvas = document.getElementById('canvas'); 
+    // const context = canvas.getContext('2d'); 
+    
+    // // setup and draw the blank canvas 
     useEffect(() => {
         const canvas = document.getElementById("canvas");
-        const ctx = canvas.getContext("2d");
-        let newGameboard = []; 
+        const context = canvas.getContext("2d");
+        setCanvasState({ canvas: canvas, context: context})
         
-        ctx.fillStyle = 'rgb(8,8,8)';
-        ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-
+        context.fillStyle = 'rgb(8,8,8)';
+        context.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+        
         // ctx.fillStyle = Cell.ALIVE_COLOR; 
         // ctx.fillRect(50, 50, 10, 10); 
+
         // let test = new Cell(ctx, 50, 50); 
-        // ctx.fillStyle = Cell.ALIVE_COLOR; 
         // test.drawHexagonCell(ctx, 150, 150); 
         // ctx.fill(); 
-        return ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    }, [gameboard]); 
 
-
+        // setGameboard(newGameboard);
+        // return ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+        }, []); // ensure this function only runs once - only draw the canvas once
         
-    return (
-        <>
+        // initialize cells and the gameboard
+        useEffect(() => {
+        let numRows = CANVAS_HEIGHT / Cell.height;
+        let numCols = CANVAS_WIDTH / Cell.width;
+        let nextLifecycle = []; 
+            for (let y = 0; y < numRows; y++) {
+                for (let x = 0; x < numCols; x++) {
+                    nextLifecycle.push(new Cell(canvasState.context, x, y));
+                }
+            }
+            console.log(gameboard); 
+            // console.log(canvasState); 
+            setGameboard(nextLifecycle); 
+        }, [canvasState]); 
+        
+        
+        return (
+            <>
         {/* <h1>This is the gameboard. We&apos;re gonna do Canvas for this one...</h1> */}
         <canvas id='canvas' width={CANVAS_WIDTH} height={CANVAS_HEIGHT} className=''></canvas>
         </>
